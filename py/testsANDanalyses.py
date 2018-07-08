7509c0c6ebba9cc1916881f00c0e3dcf2139cc10
ec <varikvi@yahoo.com>
2018-07-08 16:42:54 +0200

    testing topic modelling methods

--- py/testsANDanalyses.py
+++ py/testsANDanalyses.py
@@ -9,6 +9,12 @@ import time
 import processingData
 import collections, itertools, copy, operator
 
+import nltk
+import pandas
+import gensim
+import sklearn
+import re
+import string
 
 ##################
 ## Previous data Preparation
@@ -33,8 +39,50 @@ print(len(otp))
 data = [{ "user": k, "data": otp[k] }  for k in otp]
 print(len(data))
 al, nor, fd = processingData.allrecordsLemmatization(processingData.allrecordsPreparation(data))
+wordimportance = processingData.wordimportance_var2(nor,fd)
+
+def weighted_lda(norm_posedsts, all_fd, wordimportance):
+    NUM_TOPICS = 10
+    STOPWORDS = nltk.corpus.stopwords.words('english')
+    redo_corpus_by_sts = []
+    for norm_t in norm_posedsts:
+        st = []
+        for norm_w in norm_t:
+            if norm_w in STOPWORDS:
+                continue
+            if norm_w != '.':
+                if re.match(r'\w+', norm_w):
+                    st.append(norm_w)
+            else:
+                redo_corpus_by_sts.append(st)
+        if norm_w != '.':
+            redo_corpus_by_sts.append(st)
+    dictionary = gensim.corpora.Dictionary(redo_corpus_by_sts)
+    corpus = [dictionary.doc2bow(text) for text in redo_corpus_by_sts]
+    
+    print(len(redo_corpus_by_sts))
+    
+    lda_model = gensim.models.LdaModel(corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary)
+    lsi_model = gensim.models.LsiModel(corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary)
+
+   
+    return lda_model, lsi_model
+
+def showmodelresults(model, NUM_TOPICS=10):
+    for idx in range(NUM_TOPICS):
+        # Print the first 10 most representative topics
+        print("Topic #%s:" % idx, model.print_topic(idx, 20))
+        print("=" * 20)
+
+lda_model, lsi_model = weighted_lda(nor, fd, wordimportance)
+
+showmodelresults(lda_model)
+showmodelresults(lsi_model)
+            
+    
+    
 #fn = processingData.jsonbuilding(al, nor, fd)
 
 #print(sorted(all_fd.items(), key=lambda x: x[1], reverse=True)[round(len(all_fd)/50):round(len(all_fd)/50)+10+1])
 
-opacity, sizing, enlargedopacity = processingData.metrics_test(al, nor, fd)
+#opacity, sizing, enlargedopacity = processingData.metrics_test(al, nor, fd)
